994bf2c8f927b3592c35ab252ba880e77f39e39f
Theodore Chang <tlcfem@gmail.com>
2025-09-27 22:01:28 +0200

    Add balloon cyclic analysis scripts for material and element definitions

---
+++ PIC/CYCLIC/balloon.cyclic.py
@@ -0,0 +1,131 @@
+import os
+import subprocess
+from matplotlib.collections import LineCollection
+from matplotlib.colors import Normalize
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({"font.size": 6})
+
+
+def new_fig():
+    fig = plt.figure(figsize=(6, 2.5), tight_layout=True)
+    ax1 = fig.add_subplot(111)
+    ax1.grid(True)
+    return fig, ax1
+
+
+def plot_gradient_line(
+    x,
+    y,
+    cmap="viridis",
+    linewidth=2,
+    marker: str | None = "x",
+    markevery=149,
+):
+    x = np.asarray(x)
+    y = np.asarray(y)
+    z = np.arange(len(x))
+
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+
+    lc = LineCollection(
+        np.concatenate([points[:-1], points[1:]], axis=1),
+        cmap=cmap,
+        norm=Normalize(z.min(), z.max()),
+    )
+    lc.set_array(z)
+    lc.set_linewidth(linewidth)
+
+    # Plot
+    fig, ax = new_fig()
+    ax.add_collection(lc)
+    ax.set_xlim(x.min(), x.max())
+    ax.set_ylim(y.min(), y.max())
+
+    # Add optional markers
+    if marker:
+        ax.plot(
+            x[::markevery],
+            y[::markevery],
+            linestyle="None",
+            marker=marker,
+            color="black",
+        )
+
+    return fig, ax
+
+
+SUANPAN_EXE = (
+    ("C:\\Users\\Theodore\\Documents\\Repos\\suanPan\\MSVC\\Release\\suanPan.exe")
+    if os.name == "nt"
+    else "suanpan"
+)
+
+
+def plot():
+    global SUANPAN_EXE
+    if not SUANPAN_EXE:
+        if subprocess.run(["which", "suanpan"]).returncode != 0:
+            print("suanPan not found, please install it first.")
+            return
+        SUANPAN_EXE = "suanpan"
+
+    subprocess.run([SUANPAN_EXE, "-f", "balloon.cyclic.sp"], stdout=subprocess.DEVNULL)
+
+    strain = np.loadtxt("R3-E1.txt")
+    stress = np.loadtxt("R2-S1.txt")
+    hist = np.loadtxt("R1-HIST1.txt")
+
+    fig, ax = new_fig()
+
+    ax.plot(
+        strain[:, 1] * 1e3, stress[:, 1], color="#d73027", marker="x", markevery=149
+    )
+    ax.legend(["$\\sigma$"], loc="upper left")
+    ax.set_xlabel("strain ($1/1000$)")
+    ax.set_ylabel("stress (MPa)")
+    ax.set_xbound(min(strain[:, 1] * 1e3), max(strain[:, 1] * 1e3))
+    ax.set_ybound(1.1 * min(stress[:, 1]), 1.1 * max(stress[:, 1]))
+
+    fig.savefig("cyclic.total.pdf")
+
+    fig, ax = plot_gradient_line(
+        strain[:, 1] * 1e3, stress[:, 1], marker=None, cmap="rainbow"
+    )
+    ax.set_xlabel("strain ($1/1000$)")
+    ax.set_ylabel("stress (MPa)")
+    fig.savefig("cyclic.gradient.pdf")
+
+    fig, ax = new_fig()
+
+    ax.plot(strain[:, 1] * 1e3, hist[:, 6], color="#d73027", marker="x", markevery=149)
+    ax.legend(["$z$"], loc="upper left")
+    ax.set_xlabel("strain ($1/1000$)")
+    ax.set_ylabel("normal yield ratio $z$")
+    ax.set_xbound(min(strain[:, 1] * 1e3), max(strain[:, 1] * 1e3))
+    ax.set_ybound(0, 1.1)
+
+    fig.savefig("cyclic.ratio.total.pdf")
+
+    turning_points = []
+    for i in range(1, len(stress[:, 1]) - 1):
+        if (stress[i - 1, 1] < stress[i, 1] > stress[i + 1, 1]) or (
+            stress[i - 1, 1] > stress[i, 1] < stress[i + 1, 1]
+        ):
+            turning_points.append(stress[i, 1])
+
+    fig, ax = new_fig()
+    ax.plot(
+        range(len(turning_points)),
+        turning_points,
+        color="#d73027",
+        marker="x",
+        linestyle="None",
+    )
+    fig.savefig("cyclic.stress.total.pdf")
+
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    plot()
---
+++ PIC/CYCLIC/balloon.cyclic.sp
@@ -0,0 +1,43 @@
+node 1 0 0
+node 2 1 0
+
+material Balloon1D 1 \
+2E5 5E3 .999 1 \
+200 1E3 0 0 \
+0 -5E2 0 1E2 \
+0 0 0 0 \
+5E3 1. \
+100 0.7
+
+element T2D2 1 1 2 1 1
+
+plainrecorder 1 Element HIST 1
+plainrecorder 2 Element S 1
+plainrecorder 3 Element E 1
+
+fix2 1 1 1
+fix2 2 2 1 2
+
+expression SimpleScalar 1 t 0.5-0.5*cos(6.28318530718*t)
+
+amplitude Tabular 1 cyclic
+
+amplitude Sine 2 1 1
+
+amplitude Custom 3 1
+
+# cload 1 2 200 1 2
+disp 1 2 2e-3 1 2
+
+step static 1 1000
+set fixed_step_size 1
+set ini_step_size 1E-2
+set symm_mat 0
+
+converger RelIncreDisp 1 1E-10 10 1
+
+analyze
+
+save recorder 1 2 3
+
+exit
