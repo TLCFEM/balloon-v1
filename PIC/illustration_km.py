33de1d8706519dfc6caa26985f803357d871674d
Theodore Chang <tlcfem@gmail.com>
2025-10-25 22:54:41 +0200

    Refactor K_m function for clarity and update FLEX.tex to enhance loading phase explanation

--- PIC/illustration_km.py
+++ PIC/illustration_km.py
@@ -30,34 +30,31 @@ def line_style_generator():
 
 
 def K_m(z, z_r, k_r):
-    return 1 - np.exp((z - z_r) / k_r / (z - 1.0))
+    return 1 - np.exp((1 - z_r) / k_r * z / (z - 1))
 
 
 if __name__ == "__main__":
     os.chdir(Path(__file__).parent)
 
-    z_r = 0.35
-
     ls_gen = line_style_generator()
     plt.figure(figsize=(5, 2))
-    for k_r in [1e-1, 0.2, 0.5, 1.0, 2.0, 5, 1e1]:
-        z = np.linspace(z_r, 1.0, 80, endpoint=False)
-        plt.plot(z, K_m(z, z_r, k_r), linestyle=next(ls_gen))
-
     ax = plt.gca()
-    ax.set_xticks([0, 1])
-    ax.set_xticklabels(["0.0", "1.0"])
-    ax.text(0.35, -0.05, r"$z_r$", ha="center", va="top", transform=ax.transAxes)
+
+    for k_r in [0.5, 1, 2, 5, 1e1]:
+        z = np.linspace(0, 1.0, 80, endpoint=False)
+        plt.plot(z, K_m(z, 0.5, k_r), linestyle=next(ls_gen), color="#ca0020")
+
     ax.annotate(
-        r"$k_r$",
+        r"$k_r\uparrow~z_r\uparrow$",
         xy=(0.9, 0.2),
         xytext=(0.3, 0.7),
-        arrowprops=dict(arrowstyle="<|-|>", lw=1.5, color="red"),
+        arrowprops=dict(arrowstyle="-|>", lw=1.5, color="#0571b0"),
     )
 
     plt.xlabel(r"$z$")
-    plt.ylabel(r"$K_m(z)$")
+    plt.ylabel(r"$K_m$")
     plt.xlim(0, 1)
     plt.ylim(0, 1)
+    plt.grid(True, linestyle="--", linewidth=0.5)
     plt.tight_layout()
     plt.savefig("illustration_km.pdf")
