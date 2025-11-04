889c25a8aa1f8320590cf14bde3d6fa52e0059e9
Theodore Chang <tlcfem@gmail.com>
2025-11-04 15:10:26 +0100

    Update material parameters in cs1018.3.py for accuracy and adjust PDF outputs

--- PIC/CYCLIC/cs1018.3.py
+++ PIC/CYCLIC/cs1018.3.py
@@ -13,14 +13,14 @@ node 2 1 0
 
 material Balloon1D 1 \
 200 1e2 2 \
-4E3 0 -3.96E3 2e3 \ ! u
-.7 4 0 0 \ ! hfm
-0 0 -.2 1e3 \ ! hfc
-.2 4 0 0 \ ! ham
-0 0 0 0 \ ! hac
-5E0 1. \ ! fc
-5E0 1. \ ! ac
-1E2 1. \ ! na
+4E3 0 -3.6E3 1e3 \ ! u
+.6 1 0 0 \ ! hfm
+0 0 -.1 1e2 \ ! hfc
+.1 0 0 0 \ ! ham
+0 0 -.05 1e2 \ ! hac
+5E1 1. \ ! fc
+5E2 1. \ ! ac
+5E1 1. \ ! na
 1E2 .8 ! nd
 
 element T2D2 1 1 2 1 1
